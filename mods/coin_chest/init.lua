2c7a003777efb9fbb8ff5f8a3f5de058ea2d4463
David Leal <halfpacho@gmail.com>
2022-05-05 01:44:22 -0500

    Update (still WIP)

--- mods/coin_chest/init.lua
+++ mods/coin_chest/init.lua
@@ -1,36 +1,48 @@
-local storage = minetest.get_mod_storage()
+local global_fields = {}
+local my_boolean = false
 
-----------------
--- Formspecs --
-----------------
-local function show_formspec(name)
+----------------------
+-- Local functions --
+----------------------
+local function show_formspec(meta)
 	local text = "Choose the options here."
 	local formspec = {
 		"formspec_version[4]",
 		"size[7,5]",
 		"label[1.25,0.5;", minetest.formspec_escape(text), "]",
-		"field[0.375,1.2;5.25,0.8;bronze;Bronze coins;0]",
-		"field[0.375,2.13;5.25,0.8;silver;Silver coins;0]",
-		"field[0.375,3;5.25,0.8;silver;Gold coins;0]",
-		"button_exit[2,4;3,0.8;apply;Apply changes]"
+		"field[0.375,1.2;5.25,0.8;bronze;Bronze coins;${bronze}]",
+		"field[0.375,2.13;5.25,0.8;silver;Silver coins;${silver}]",
+		"field[0.375,3;5.25,0.8;gold;Gold coins;${gold}]",
+		"button_exit[4,3.90;3,0.8;apply;Apply changes]",
+		"checkbox[0.375,4.5;staff_coins;Give coins to staff;", (meta:get_int("staff_coins") == 1) and "true" or "false", "]"
 	}
 
     -- table.concat is faster than string concatenation - `..`
     return table.concat(formspec, "")
 end
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname ~= "coin_chest:options" then
-		return
-	end
+local function meta_checks(clicker, pos)
+	local meta = minetest.get_meta(pos)
+	if not meta then
+		return false, "You don't have the sufficient permissions to open this chest. Missing privileges: core_admin"
+	else
+		meta:set_string("formspec", "")
 
-	if fields.bronze then
-		local name = player:get_player_name()
-		local context = get_context(name)
-		context.guess = tonumber(fields.number)
-		show_to(name)
+		meta = clicker:get_meta()
+		local coins = minetest.deserialize(meta:get_string("player_coins"))
+
+		if coins then
+			coins.bronze_coins = tonumber(coins.bronze_coins + global_fields.bronze)
+			coins.silver_coins = tonumber(coins.silver_coins + global_fields.silver)
+			coins.gold_coins = tonumber(coins.gold_coins + global_fields.gold)
+
+			meta:set_string("player_coins", minetest.serialize(coins))
+		else
+			coins = { bronze_coins = global_fields.bronze, silver_coins = global_fields.silver, gold_coins = global_fields.gold }
+			meta:set_string("player_coins", minetest.serialize(coins))
+		end
 	end
-end)
+end
 
 -------------
 -- Nodes --
@@ -54,21 +66,66 @@ minetest.register_node("coin_chest:chest", {
         end
 		return minetest.item_place(itemstack, placer, pointed_thing)
 	end,
-    after_place_node = function(pos, placer, itemstack, pointed_thing)
-        if not minetest.check_player_privs(placer, { core_admin = true }) then
-            return false, "You don't have the sufficient permissions to place this node. Missing privileges: core_admin"
-        end
-		minetest.show_formspec(placer:get_player_name(), "coin_chest:options", show_formspec(placer))
+	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		if not minetest.check_player_privs(clicker, { core_admin = true }) then
+			if not meta then
+            	return false, "You don't have the sufficient permissions to open this chest. Missing privileges: core_admin"
+			else
+				meta:set_string("formspec", "")
 
+				meta = clicker:get_meta()
+				local coins = minetest.deserialize(meta:get_string("player_coins"))
+
+				if coins then
+					coins.bronze_coins = coins.bronze_coins + global_fields.bronze
+					coins.silver_coins = coins.silver_coins + global_fields.silver
+					coins.gold_coins = coins.gold_coins + global_fields.gold
+
+					meta:set_string("player_coins", minetest.serialize(coins))
+				else
+					coins = { bronze_coins = global_fields.bronze, silver_coins = global_fields.silver, gold_coins = global_fields.gold }
+					meta:set_string("player_coins", minetest.serialize(coins))
+				end
+			end
+		else
+			--if not meta then return end
+			--meta_checks(clicker, pos)
+        end
+	end,
+	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		meta:set_string("infotext", "Chest");
-    end,
-	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-		if not minetest.check_player_privs(clicker, { core_admin = true }) then
-            return false, "You don't have the sufficient permissions to place this node. Missing privileges: core_admin"
-        end
-		minetest.show_formspec(clicker:get_player_name(), "coin_chest:options", show_formspec(clicker))
+		meta:set_string("formspec", show_formspec(meta))
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		local meta = minetest.get_meta(pos)
+		global_fields = fields -- Set values as fast as possible for the formspec checkbox (see code above)
+		if fields.apply then
+			if fields.bronze == "" or fields.bronze == "0" or fields.silver == "" or
+				fields.silver == "0" or fields.gold == "" or fields.gold == "0" then
+				minetest.chat_send_player(sender:get_player_name(), "Please specify a valid value different than zero.")
+				return
+			end
+
+			meta:set_string("bronze", fields.bronze)
+			meta:set_string("silver", fields.silver)
+			meta:set_string("gold", fields.gold)
+
+			global_fields = fields
+			minetest.chat_send_player(sender:get_player_name(), "Successfully updated/set coin chest!")
+			--minetest.log("info", fields.staff_coins)
+		end
+		if fields.staff_coins then
+			if my_boolean == true then
+				my_boolean = false
+				meta:set_int("staff_coins", (my_boolean == true) and 1 or 0)
+				minetest.chat_send_all((meta:get_int("staff_coins") == 1) and "true" or "false")
+				return
+			end
+			my_boolean = true
+			meta:set_int("staff_coins", (my_boolean == true) and 1 or 0)
+			minetest.chat_send_all((meta:get_int("staff_coins") == 1) and "true" or "false")
+		end
 	end,
 })
-
-
